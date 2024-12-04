78542fc7423d1f4c15c0b9d74393d37e569953b9
dhanashree-mhatre <dhanashree.m@rejolut.com>
2024-12-04 18:44:12 +0530

    aaded so files to push

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import os
 import shutil
 import glob
-# Helper function to find all .py files, excluding __init__.py
+
 def find_pyx_files(directory):
     pyx_files = []
     for root, dirs, files in os.walk(directory):
@@ -13,17 +13,18 @@ def find_pyx_files(directory):
     return pyx_files
 
 # Specify the directories you want to convert to .so
-directories = ['rejoai']
+directories = ['rejo_ai']
 
 # Collect all .py files
-pyx_files = []
-for directory in directories:
-    pyx_files.extend(find_pyx_files(directory))
+pyx_files = find_pyx_files('rejo_ai')
 
-# Define the extensions
-extensions = [Extension(
-    pyx.replace('.py', '').replace('/', '.'),
-    [pyx]) for pyx in pyx_files]
+# Define the extensions with corrected module paths
+extensions = [
+    Extension(
+        f'rejo_ai.{os.path.splitext(os.path.basename(pyx))[0]}',
+        [pyx]
+    ) for pyx in pyx_files
+]
 
 setup(
     name='rejoai',
@@ -32,10 +33,12 @@ setup(
     description='my_project',
     author='author',
     license='Proprietary',
-    install_requires=[
-        'cython',
-    ],
-    ext_modules=cythonize(extensions,compiler_directives={'language_level': 3}),
+    setup_requires=['cython'],
+    install_requires=['cython'],
+    ext_modules=cythonize(
+        extensions, 
+        compiler_directives={'language_level': 3}
+    ),
 )
 
 # Copy __init__.py files to build_package directory
@@ -43,16 +46,17 @@ for directory in directories:
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file == '__init__.py':
-                dest_dir = root.replace('rejoai', 'build_package/rejoai')
+                dest_dir = root.replace('rejo_ai', 'build_package/rejo_ai')
                 os.makedirs(dest_dir, exist_ok=True)
                 shutil.copy(os.path.join(root, file), os.path.join(dest_dir, file))
 
 # Copy .so files to build_package directory
-for so_file in glob.glob('rejoai/**/*.so', recursive=True):
-    dest_file = so_file.replace('core', 'build_package/rejoai')
+for so_file in glob.glob('rejo_ai/**/*.so', recursive=True):
+    dest_file = so_file.replace('rejo_ai', 'build_package/rejo_ai')
     dest_dir = os.path.dirname(dest_file)
     os.makedirs(dest_dir, exist_ok=True)
-    shutil.move(so_file, dest_file)
+    shutil.move(so_file, dest_file)     
+                                                                                                                                                                                                                                                       
 
 # Cleanup .c files from core directory
 for directory in directories:
@@ -62,4 +66,7 @@ for directory in directories:
                 os.remove(os.path.join(root, file))
 
 # Remove the build folder
-shutil.rmtree('build', ignore_errors=True)
+shutil.rmtree('build', ignore_errors=True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+
+
