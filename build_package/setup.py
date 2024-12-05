2357d51e1d4b4140b74744628638daa43d6cfd5e
dhanashree-mhatre <dhanashree.m@rejolut.com>
2024-12-05 10:23:20 +0530

    fixed setup file

--- build_package/setup.py
+++ build_package/setup.py
@@ -2,12 +2,19 @@ from setuptools import setup, find_packages
 from setuptools import find_packages, setup, Extension
 from Cython.Build import cythonize
 
+def read_readme():
+    try:
+        with open("README.md", "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        return "A brief description of the package"
+
 setup(
     name="rejoai",                       # Package name
     version="0.1.9",                         # Initial version
     description="A brief description",       # Short description
-    long_description=open("README.md").read(),  # Detailed description from README
-    long_description_content_type="text/markdown",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown", 
     author="RejoAI Team",                      # Author name
     author_email="your.email@example.com",   # Author email
     url="https://github.com/rejoai",  # Project URL
